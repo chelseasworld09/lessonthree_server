36b6d2982435cb1bd9b725039f6ee25851bb31da
Chelsea Sanders <you@example.com>
2018-04-28 12:16:52 -0700

    lessonthreehw

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.post("/", function(req, res) {
+  res.send("Data can be created using this method (POST).");
+});
+
+app.get("/", function(req, res) {
+  res.send("GET is used to read information.");
+});
+
+app.put("/", function(req, res) {
+  res.send("Use this (PUT) to update information.");
+});
+
+app.delete("/", function(req, res) {
+  res.send("Only use DELETE to remove the specified target.");
+});
+
+app.listen(3000, () => {
+  console.log("it works!");
+});
